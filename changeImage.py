23ac558d779cd2d436207be31fe083aa9f22d0f4
DmitriiDes <26392450+DmitriiDes@users.noreply.github.com>
2021-03-12 07:26:26 -0500

    Added: 	new file:   changeImage.py Image converter script to reformat sourse images to desired format and saved in selected directory 	new file:   supplier-data.tar.gz Archive with the original images. Used to restore sourse images to the original state.

---
+++ changeImage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+import fnmatch
+import time
+
+def get_list_of_files_from_directory(sourse_directory, extension):
+    """
+    Returns a list of files with a specified extension from the sourse sourse_directory
+    """
+    file_list = fnmatch.filter(os.listdir(sourse_directory), extension)
+    return file_list
+
+def convert_save_img(img_name, sourse_directory, target_directory, img_format, img_size, img_extension):
+    """
+    Converts an image from a sourse_directory to a specified size and format and
+    saves it into target_directory with img_extension
+    """
+    img = Image.open(sourse_directory + "/" + img_name)
+    img_name = os.path.splitext(img_name)[0]
+    img = img.resize(img_size).convert(img_format)
+    img.save(target_directory + "/" + img_name + "." + img_extension, img_extension, quality=100)
+    img.close()
+
+def convert_img(sourse_directory, target_directory, source_img_extension, img_format, img_size, img_extension):
+    """
+    Converts all images from sourse_directory with source_img_extension to a specified size and format and
+    saves it into target_directory
+    """
+    img_list = get_list_of_files_from_directory(sourse_directory, source_img_extension)
+    for img_name in img_list:
+        convert_save_img(img_name, sourse_directory, target_directory, img_format, img_size, img_extension)
+
+
+
+if __name__ == '__main__':
+    sourse_directory = "./supplier-data/images"
+    target_directory = "./supplier-data/images"
+    source_img_extension = "*.tiff"
+    img_extension = "jpeg"
+    img_format = "RGB"
+    img_size = (600, 400)
+    start_time = time.time()
+    convert_img(sourse_directory,target_directory, source_img_extension, img_format, img_size, img_extension)
+    print("--- %s seconds ---" % (time.time()-start_time))
