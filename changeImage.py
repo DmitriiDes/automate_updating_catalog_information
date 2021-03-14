9230be0a7701cce4cceb8d2ed99d80620ffc1ec7
DmitriiDes <26392450+DmitriiDes@users.noreply.github.com>
2021-03-14 08:05:08 -0400

    Add: supplier_image_upload script to upload images to the server Edit: edit code to match PEP 8 Style Guidlines

--- changeImage.py
+++ changeImage.py
@@ -7,30 +7,40 @@ import time
 
 def get_list_of_files_from_directory(sourse_directory, extension):
     """
-    Returns a list of files with a specified extension from the sourse sourse_directory
+    Returns a sorted list of files with a specified extension from
+    the sourse sourse_directory
     """
     file_list = fnmatch.filter(os.listdir(sourse_directory), extension)
+    file_list.sort()
     return file_list
 
-def convert_save_img(img_name, sourse_directory, target_directory, img_format, img_size, img_extension):
+def convert_save_img(img_name, sourse_directory, target_directory,
+                     img_format, img_size, img_extension):
     """
     Converts an image from a sourse_directory to a specified size and format and
     saves it into target_directory with img_extension
     """
     img = Image.open(sourse_directory + "/" + img_name)
+    #Get the file name without extension
     img_name = os.path.splitext(img_name)[0]
+    #Convert image into desired format
     img = img.resize(img_size).convert(img_format)
-    img.save(target_directory + "/" + img_name + "." + img_extension, img_extension, quality=100)
+    #Save image with the desired extension
+    img.save(target_directory + "/" + img_name + "." + img_extension,
+             img_extension, quality=100)
     img.close()
 
-def convert_img(sourse_directory, target_directory, source_img_extension, img_format, img_size, img_extension):
+def convert_img(sourse_directory, target_directory, source_img_extension,
+                img_format, img_size, img_extension):
     """
-    Converts all images from sourse_directory with source_img_extension to a specified size and format and
-    saves it into target_directory
+    Converts all images from sourse_directory with source_img_extension to
+    a specified size and format and saves it into target_directory
     """
-    img_list = get_list_of_files_from_directory(sourse_directory, source_img_extension)
+    img_list = get_list_of_files_from_directory(sourse_directory,
+                                                source_img_extension)
     for img_name in img_list:
-        convert_save_img(img_name, sourse_directory, target_directory, img_format, img_size, img_extension)
+        convert_save_img(img_name, sourse_directory,
+                         target_directory, img_format, img_size, img_extension)
 
 
 
@@ -42,5 +52,6 @@ if __name__ == '__main__':
     img_format = "RGB"
     img_size = (600, 400)
     start_time = time.time()
-    convert_img(sourse_directory,target_directory, source_img_extension, img_format, img_size, img_extension)
+    convert_img(sourse_directory,target_directory, source_img_extension,
+                img_format, img_size, img_extension)
     print("--- %s seconds ---" % (time.time()-start_time))
---
+++ supplier_image_upload.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+from changeImage import get_list_of_files_from_directory
+
+def upload_file_to_the_server(file_path, server_url):
+    """
+    Uploads file from file_path to a server specified by server_url
+    """
+    with open(file_path, 'rb') as opened_file:
+        r = requests.post(server_url, files={file: opened_file})
+
+def get_full_path_list_of_files(sourse_directory, file_extension):
+    """
+    Returns the list of full paths to the files with specified file_extension
+    from the sourse_directory
+    """
+    full_path_list = []
+    file_list = get_list_of_files_from_directory(sourse_directory,
+                                                 file_extension)
+    for file_name in file_list:
+        full_path = os.path.join(sourse_directory, file_name)
+        if os.path.isfile(full_path):
+            full_path_list.append(full_path)
+    return full_path_list
+
+def upload_all_from_the_list_to_url(full_path_list, server_url):
+    """
+    Uploads all existing files from the list to the server url
+    """
+    for file_full_path in full_path_list:
+        if os.path.isfile(file_full_path):
+            try:
+                upload_file_to_the_server(file_full_path, server_url)
+            except:
+                print(
+                    "Upload error: {} wasn't loaded to the server {}".
+                        format(file_full_path, server_url)
+                    )
+
+def upload_all_from_the_sourse_directory(sourse_directory,
+                                         file_extension, server_url):
+    """
+    Uploads all files with specified file_extension from a specified
+    sourse_directory to a server
+    """
+    full_path_list = get_full_path_list_of_files(sourse_directory,
+                                                 file_extension)
+    upload_all_from_the_list_to_url(full_path_list, server_url)
+
+if __name__ == '__main__':
+    server_url = "http://localhost/upload/"
+    sourse_directory = "./supplier-data/images"
+    file_extension = "*.jpeg"
+    upload_all_from_the_sourse_directory(sourse_directory,
+                                         file_extension, server_url)
